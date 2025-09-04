e7b68c71116bc1deeb0f5bfa5516eae9c9b9f768
DJY <954128653@qq.com>
2025-09-04 21:08:51 +0800

    init

--- model_sample.py
+++ model_sample.py
@@ -2,13 +2,13 @@ import os
 import pickle
 from contextlib import nullcontext
 import torch
-from k_model import ModelConfig, Transformer
+from ModelConfig import ModelConfig, Transformer
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import argparse
 
 class TextGenerator:
     def __init__(self, 
-                 checkpoint='./base_model_215M/pretrain_1024_18_6144.pth',  # 模型检查点路径
+                 checkpoint='./base_model_215M/sft.pth',  # 模型检查点路径
                  tokenizer_model_path='./tokenizer_k/',  # 分词器模型路径
                  seed=42,  # 随机种子，确保可重复性
                  device=None,  # 设备，优先使用 CUDA，如果没有可用的 CUDA，则使用 CPU
@@ -129,11 +129,11 @@ if __name__ == "__main__":
     print("------------------- Pretrain Sample ------------------- \n")
 
     pretrain_prompt_datas = [
-        '<|im_start|>北京大学是',
-        '<|im_start|>中国矿业大学（北京）地球科学与测绘工程学院',
+        '<|im_start|>北京大学很',
+        '<|im_start|>中国科学技术大学是',
     ]
 
-    generator = TextGenerator(checkpoint='./base_model_215M/pretrain_1024_18_6144.pth')  # 初始化生成器
+    generator = TextGenerator(checkpoint='./base_model_215M/pretrain.pth')  # 初始化生成器
     for i in range(len(pretrain_prompt_datas)):
         samples = generator.pretrain_sample(start=pretrain_prompt_datas[i], num_samples=1, max_new_tokens=120, temperature=0.75)
         print(f"\nSample {i+1}:\n{pretrain_prompt_datas[i]}{samples[0]}\n{'-'*20}")  # 打印生成的样本并用分隔线分割
@@ -143,10 +143,10 @@ if __name__ == "__main__":
     sft_prompt_datas = [
         '你好呀',
         "中国的首都是哪里？",
-        "1+12等于多少？",
+        "介绍下中国科学技术大学",
         "你是谁？"
     ]
-    generator = TextGenerator(checkpoint='./sft_model_215M/sft_dim1024_layers18_vocab_size6144.pth')  # 初始化生成器
+    generator = TextGenerator(checkpoint='./base_model_215M/sft.pth')  # 初始化生成器
     for i in range(len(sft_prompt_datas)):
         samples = generator.sft_sample(start=sft_prompt_datas[i], num_samples=1, max_new_tokens=128, temperature=0.6)
         print(f"\nSample {i+1}:\nQuestion: {sft_prompt_datas[i]} \nAI answer: {samples[0]}\n{'-'*20}")  # 打印生成的样本并用分隔线分割
